d73cfcc47ba7518789d584849d991835d6bdac89
G'lek Tarssza <glek@glektarssza.com>
2026-02-26 08:02:03 -0700

    Some doc comments.

--- Glek.VintageStory.AFKMod/src/AFKModSystem.cs
+++ Glek.VintageStory.AFKMod/src/AFKModSystem.cs
@@ -5,15 +5,34 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
+/// <summary>
+/// The mod system.
+/// </summary>
 public class AFKModSystem : ModSystem {
-    // Called on server and client
-    // Useful for registering block/entity classes on both sides
+    /// <summary>
+    /// The startup method, called on both the server and client.
+    /// </summary>
+    /// <param name="api">
+    /// The core API object.
+    /// </param>
     public override void Start(ICoreAPI api) =>
         this.Mod.Logger.Notification("Hello from template mod: " + api.Side);
 
+    /// <summary>
+    /// The startup method, called on the server only.
+    /// </summary>
+    /// <param name="api">
+    /// The core API object.
+    /// </param>
     public override void StartServerSide(ICoreServerAPI api) =>
         this.Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("gleksafkmod:hello"));
 
+    /// <summary>
+    /// The startup method, called on the client only.
+    /// </summary>
+    /// <param name="api">
+    /// The core API object.
+    /// </param>
     public override void StartClientSide(ICoreClientAPI api) =>
         this.Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("gleksafkmod:hello"));
 }
