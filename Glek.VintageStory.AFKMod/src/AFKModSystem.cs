61f8d60273237f858e7c5b3427e392f486876f6f
G'lek Tarssza <glek@glektarssza.com>
2026-03-05 09:24:09 -0700

    Main class.

--- Glek.VintageStory.AFKMod/src/AFKModSystem.cs
+++ Glek.VintageStory.AFKMod/src/AFKModSystem.cs
@@ -1,5 +1,7 @@
 namespace Glek.VintageStory.AFKMod;
 
+using Glek.VintageStory.AFKMod.API;
+
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
@@ -8,31 +10,6 @@ using Vintagestory.API.Server;
 /// <summary>
 /// The mod system.
 /// </summary>
-public class AFKModSystem : ModSystem {
-    /// <summary>
-    /// The startup method, called on both the server and client.
-    /// </summary>
-    /// <param name="api">
-    /// The core API object.
-    /// </param>
-    public override void Start(ICoreAPI api) =>
-        this.Mod.Logger.Notification("Hello from template mod: " + api.Side);
-
-    /// <summary>
-    /// The startup method, called on the server only.
-    /// </summary>
-    /// <param name="api">
-    /// The core API object.
-    /// </param>
-    public override void StartServerSide(ICoreServerAPI api) =>
-        this.Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("gleksafkmod:hello"));
-
-    /// <summary>
-    /// The startup method, called on the client only.
-    /// </summary>
-    /// <param name="api">
-    /// The core API object.
-    /// </param>
-    public override void StartClientSide(ICoreClientAPI api) =>
-        this.Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("gleksafkmod:hello"));
+public class AFKModSystem : ModSpecificModSystem {
+    // TODO
 }
