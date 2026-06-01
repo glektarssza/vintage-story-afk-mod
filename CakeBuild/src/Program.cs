b5d99dc69d154b1367a8a3299f21974e7bbf208a
G'lek Tarssza <glek@glektarssza.com>
2026-06-01 12:36:08 -0600

    Ensure publish is correct.

--- CakeBuild/src/Program.cs
+++ CakeBuild/src/Program.cs
@@ -80,18 +80,18 @@ public sealed class BuildTask : FrostingTask<BuildContext> {
 [IsDependentOn(typeof(BuildTask))]
 public sealed class PackageTask : FrostingTask<BuildContext> {
     public override void Run(BuildContext context) {
-        context.EnsureDirectoryExists("../Releases");
-        context.CleanDirectory("../Releases");
-        context.EnsureDirectoryExists($"../Releases/{context.Name}");
-        context.CopyFiles($"../{BuildContext.ProjectName}/bin/{context.BuildConfiguration}/Mods/mod/publish/*", $"../Releases/{context.Name}");
+        context.EnsureDirectoryExists("../dist");
+        context.CleanDirectory("../dist");
+        context.EnsureDirectoryExists($"../dist/{context.Name}");
+        context.CopyFiles($"../{BuildContext.ProjectName}/bin/{context.BuildConfiguration}/Mods/mod/publish/*", $"../dist/{context.Name}");
         if (context.DirectoryExists($"../{BuildContext.ProjectName}/assets")) {
-            context.CopyDirectory($"../{BuildContext.ProjectName}/assets", $"../Releases/{context.Name}/assets");
+            context.CopyDirectory($"../{BuildContext.ProjectName}/assets", $"../dist/{context.Name}/assets");
         }
-        context.CopyFile($"../{BuildContext.ProjectName}/modinfo.json", $"../Releases/{context.Name}/modinfo.json");
+        context.CopyFile($"../{BuildContext.ProjectName}/modinfo.json", $"../dist/{context.Name}/modinfo.json");
         if (context.FileExists($"../{BuildContext.ProjectName}/modicon.png")) {
-            context.CopyFile($"../{BuildContext.ProjectName}/modicon.png", $"../Releases/{context.Name}/modicon.png");
+            context.CopyFile($"../{BuildContext.ProjectName}/modicon.png", $"../dist/{context.Name}/modicon.png");
         }
-        context.Zip($"../Releases/{context.Name}", $"../Releases/{context.Name}_{context.Version}.zip");
+        context.Zip($"../dist/{context.Name}", $"../dist/{context.Name}-v{context.Version}-{context.BuildConfiguration.ToLowerInvariant()}.zip");
     }
 }
 
