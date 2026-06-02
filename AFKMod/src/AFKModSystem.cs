ea3b347d37abde6387aded69102f832c8409bbe6
G'lek Tarssza <glek@glektarssza.com>
2026-06-01 19:17:31 -0600

    HACK: Temporarily disable IDE0005 to work around GitHub Actions warning about it during linting.

--- AFKMod/src/AFKModSystem.cs
+++ AFKMod/src/AFKModSystem.cs
@@ -2,4 +2,7 @@ namespace GlekTarssza.VintageStory.AFKMod;
 
 using Vintagestory.API.Common;
 
+/// <summary>
+/// The main mod system integration class.
+/// </summary>
 public class AFKModSystem : ModSystem { }
